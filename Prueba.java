821753f1588efb3a7ba509647f22840f0e71b9c0
root <root@Xubuntu.myguest.virtualbox.org>
2023-09-27 13:09:53 +0200

    Ahora el programa tambien imprime mi red social favorita.

--- Prueba.java
+++ Prueba.java
@@ -2,6 +2,7 @@ public class Prueba{
 	public static void main(String args[]){
 		System.out.println("Baloncesto");
 		System.out.println("Cancha de Baloncesto");
+		System.out.println("Reddit");
 	}
 
 }
